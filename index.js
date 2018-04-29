e650e1d1589c8eac51be5a826ef258ec7aa1c46b
smettu007 <subbumetu007@gmail.com>
2018-04-29 15:26:29 -0700

    fixed package.json again

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV === 'production'){
 
     //Expresss will serve index.html if it dosent recognize the route
 
-    const parth = require('path')
+    const path = require('path')  
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname, 'client','build' ,'index.html'));
     })
