5158e06c5c12718d2bb9062adb7c0284ef977cc2
Subbaramaiah Mettu <smettu@Innovation.local>
2018-04-30 15:08:18 -0700

    imporved email template

--- modals/Survey.js
+++ modals/Survey.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const recipientSchema = require('./Recepient');
+const recipientSchema = require("./Recepient");
 
 const surveySchema = new Schema({
-    title: String,
-    body:String,
-    subject:String,
-    recipients:[recipientSchema],
-    yes:{type:Number,default:0},
-    No:{type:Number,defalut:0},
-    _user: {type:Schema.Types.ObjectId, ref:'User'},
-    dateSent:Date,
-    lastResponded:Date
-})
-mongoose.model('surveys',surveySchema);
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [recipientSchema],
+  yes: { type: Number, default: 0 },
+  No: { type: Number, defalut: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  dateSent: Date,
+  lastResponded: Date
+});
+mongoose.model("surveys", surveySchema);
+
+
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,4 +1,22 @@
 module.exports = (survey)=>{
 
-    return '<div>' + survey.body +'</div>'
+    return `
+    
+            <html>
+                <div style="text-align:center;">
+
+                    <h3>I would like your input!</h3>
+                    <p>Pleae answer the following question!</p>
+                    <p>${survey.body}</p>
+                    <div>
+                        <a href="http://localhost:3000">Yes</a>
+                    </div>
+
+                    <div>
+                        <a href="http://localhost:3000">No</a>
+                    </div>
+                </div>
+            </html>
+
+    `
 }
