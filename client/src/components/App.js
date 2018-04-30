c620df561ba6f31db6003e91464e4f6527de9644
Subbaramaiah Mettu <smettu@Innovation.local>
2018-04-30 16:55:45 -0700

    added redux form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
 import {connect} from 'react-redux';
+import SurveyNew from './surveys/SurveyNew'
 import * as actions from '../actions'
 
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+
+
 
 
 class App extends Component {
@@ -18,16 +20,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
+     
         <BrowserRouter>
-          <div>
+          <div className="container">
             <Header />
             <Route exact path="/surveys" component={Dashboard} />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
-      </div>
+      
     );
   }
 }
---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,54 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+import {Link} from 'react-router-dom';
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email body", name: "body" },
+  { label: "Recipients List", name: "emails" }
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+
+          {this.renderFields()}
+
+        <Link to="/surveys" className="red btn-flat left white-text">
+            Cancel
+        </Link>
+
+
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
