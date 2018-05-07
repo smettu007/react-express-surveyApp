b82416c221aa127fec222ecc693cf8983f42a5f0
smettu007 <subbumetu007@gmail.com>
2018-05-07 09:28:32 -0700

    final

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -6,7 +6,7 @@ module.exports = (survey)=>{
                 <div style="text-align:center;">
 
                     <h3>I would like your input!</h3>
-                    <p>Pleae answer the following question!</p>
+                    <p>Please answer the following question!</p>
                     <p>${survey.body}</p>
                     <div>
                         <a href="https://evening-river-87048.herokuapp.com/api/surveys/${survey.id}/yes">Yes</a>
